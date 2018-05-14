3425f391ae38db180f8a26ac93490cf5702ea3d6
Laszlo Molnar <laszlo.molnar@codecool.com>
2018-05-14 11:54:03 +0200

    normalized logs

--- player.py
+++ player.py
@@ -3,15 +3,15 @@ class Player:
     VERSION = "0.0.2"
 
     def get_player(self, game_state, name):
-        print "02"
+        print "TOR get_player starts"
         i = 0
         while name != game_state["players"][i]["name"]:
             i = i + 1
-        print "03"
+        print "TOR get_player returns"
         return game_state["players"][i]
 
     def get_me(self, game_state):
-        print "01"
+        print "TOR get_me starts and calls get_player"
         return self.get_player(game_state, unicode("TorMentors"))
 
     def has_pairs(self, hand):
@@ -22,14 +22,12 @@ class Player:
 
     def betRequest(self, game_state):
         try:
-            print "me me me:"
+            print "TOR game_state:"
             print self.get_me(game_state)
 
             me = self.get_me(game_state)
             my_hole = me["hole_cards"]
             my_stack = me["stack"]
-            print "me me me:"
-            print self.get_me(game_state)
 
             if self.has_pairs(my_hole):
                 return my_stack
