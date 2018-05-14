088ec0d18352b613ee044c2a0b20e93f68ee9d3b
Laszlo Molnar <laszlo.molnar@codecool.com>
2018-05-14 11:24:52 +0200

    get player and me

--- player.py
+++ player.py
@@ -2,8 +2,20 @@
 class Player:
     VERSION = "0.0.1"
 
+    def get_me(game_state):
+        return get_player(game_state, "TorMentors")
+
+    def get_player(game_state, name):
+        i = 0
+        while name != game_state["players"][i]:
+            i = i + 1
+        return game_state["players"][i]
+
     def betRequest(self, game_state):
         try:
+            print "me me me:"
+            print get_me(game_state)
+
             myStack = game_state["players"]
             print '0 {0}'.format(myStack)
             
@@ -15,6 +27,7 @@ class Player:
             
             myStack = game_state["players"][0]["stack"]
             print '3 {0}'.format(myStack)
+
         except:
             print "exception occured"
         return 1000
