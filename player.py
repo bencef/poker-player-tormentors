e48d30f6261f12388dfaae9e03ffeab34fa71679
Laszlo Molnar <laszlo.molnar@codecool.com>
2018-05-14 10:36:20 +0200

    print

--- player.py
+++ player.py
@@ -1,8 +1,9 @@
 
 class Player:
-    VERSION = "Default Python folding player"
+    VERSION = "0.0.1"
 
     def betRequest(self, game_state):
+        print game_state
         return 1000
         
     def showdown(self, game_state):
