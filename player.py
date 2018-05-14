247983d1ff0ad857d39c79e72f9b2070b723c09f
Laszlo Molnar <laszlo.molnar@codecool.com>
2018-05-14 10:52:40 +0200

    logs

--- player.py
+++ player.py
@@ -3,7 +3,20 @@ class Player:
     VERSION = "0.0.1"
 
     def betRequest(self, game_state):
-        print game_state
+        try:
+            myStack = game_state["players"]
+            print '0 {0}'.format(myStack)
+            
+            myStack = game_state["players"][0]
+            print '1 {0}'.format(myStack)
+            
+            myStack = game_state["players"][0]["name"]
+            print '2 {0}'.format(myStack)
+            
+            myStack = game_state["players"][0]["stack"]
+            print '3 {0}'.format(myStack)
+        except:
+            print "exception occured"
         return 1000
         
     def showdown(self, game_state):
